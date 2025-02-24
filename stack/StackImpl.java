199badb7153d7dfbee645a6e616e42846b5ae4cb
Vincenzo Baia <vincenzo@vincenzos-mbp.fairfield.edu>
2025-02-24 11:44:55 -0500

    quick edits

--- stack/StackImpl.java
+++ stack/StackImpl.java
@@ -2,9 +2,26 @@ package stack;
 
 public class StackImpl implements Stack {
 
+	private String[] stackArray;
+	private int top;
+	private int capacity;
+
+	public StackImpl(){
+		capacity = 10;
+		stackArray = new String[capacity];
+		top = -1; //Minus because of indexing starting at 0 
+	}
+	
 	@Override
 	public void push(String s) {
-	
+		if (isFull()){
+			stackArray[++top] = s; 
+			System.out.println("Item added to stack: " + s);
+		}
+		else{
+			System.out.println("Stack is full");
+		}
+		
 		System.out.println("thing i added to stack: " + s);
 	
 	}
